83805774a0704f0d1586a3c81d1bb94ad6f92254
Marek Kożuchowski <marekkozuchowski4@gmail.com>
2023-01-09 21:40:15 +0100

    modified auth filter

--- src/main/java/com/maintenecelog/maintenancelog/filter/AuthorisationFilter.java
+++ src/main/java/com/maintenecelog/maintenancelog/filter/AuthorisationFilter.java
@@ -4,13 +4,16 @@ import com.maintenecelog.maintenancelog.model.Token;
 import com.maintenecelog.maintenancelog.service.TokenServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
-public class AuthorisationFilter implements Filter {
+public class AuthorisationFilter extends OncePerRequestFilter {
 
     private final TokenServiceImpl tokenService;
 
@@ -20,20 +23,28 @@ public class AuthorisationFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-        String auth = httpRequest.getHeader("auth").toString();
+    protected boolean shouldNotFilter(HttpServletRequest request)
+            throws ServletException {
+        String path = request.getRequestURI();
+        return "/mainteiners/logIn".equals(path) || "/mainteiners/signIn".equals(path) || "/swagger-ui/index.html".equals(path);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String auth = request.getHeader("auth");
+        LocalDateTime now = LocalDateTime.now();
         Token token = null;
         try {
             token = tokenService.getToken(auth);
-        } catch (NullPointerException e) {
+        }catch (NullPointerException e){
             e.printStackTrace();
         }
 
-        //TODO Finish filter cases
-        if(auth.equals(token.getToken())){
-            return;
+        if(token != null && now.isBefore(token.getExpiring())){
+            filterChain.doFilter(request, response);
         }
-
     }
+
 }
+
+
