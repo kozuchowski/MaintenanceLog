ac54b980db4ae7dac7ca4f0de6892faf5c37b073
Marek Kożuchowski <marekkozuchowski4@gmail.com>
2023-01-13 21:56:29 +0100

    Mainteiner model validation

--- src/main/java/com/maintenecelog/maintenancelog/model/Mainteiner.java
+++ src/main/java/com/maintenecelog/maintenancelog/model/Mainteiner.java
@@ -2,6 +2,10 @@ package com.maintenecelog.maintenancelog.model;
 
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 
 @Getter
@@ -12,11 +16,20 @@ public class Mainteiner {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String surname;
+    @NotBlank
     private String login;
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "Password minimum eight characters, at least one letter and one number")
     private String password;
+    @Email
     private String email;
+    @NotNull
+    @Pattern(regexp = "^[0-9]{10,12}", message = "Licence number must contains at least 10 and maximum 12 digits")
     private String licenceNumber;
 
 
